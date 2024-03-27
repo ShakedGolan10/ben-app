99ce0ad5574fd043cd8c1b48e238fb6b81bbef9a
Shaked Golan <shaked518@gmail.com>
2024-03-27 03:07:29 +0200

    First commit

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    swcMinify: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'dl.airtable.com',
+                pathname: '/.attachments/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'v5.airtableusercontent.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+                pathname: '/**',
+            },
+        ],
+    },
+};
+
+export default nextConfig;
