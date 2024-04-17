36d00890434f8c36d9cdac383117b4f7c44eae5b
Shaked Golan <shaked518@gmail.com>
2024-04-17 04:17:13 +0300

    REMOVING CONSOLE LOG

--- app/api/getAllMemories/route.js
+++ app/api/getAllMemories/route.js
@@ -10,13 +10,10 @@ export async function GET(req) {
 
   try {
     let stories = [];
-    console.log('the startttttt! ====>', base)
     await base("סיפורים")
       .select({ view: "מאושרים" })
       .eachPage((records, fetchNextPage) => {
-        console.log('the records! ====>', records)
         records.forEach((record) => {
-          console.log('Step 1 complete! =====)', record)
           const story = {
             id: record.get("מזהה"),
             text: record.get("תיאור"),
@@ -27,7 +24,6 @@ export async function GET(req) {
           };
           stories.push(story);
         });
-        console.log('Step 2 complete! =====)', stories)
         fetchNextPage();
       });
     return new NextResponse(JSON.stringify(stories), { status: 200 })
