75ffa80f2388d3393fec67f94a38db5118e7c12e
Shaked Golan <shaked518@gmail.com>
2024-04-17 23:14:05 +0300

    fixed main-layout bug

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en" dir='rtl'>
         <body>
           <Navbar />
-        <main className='main-layout relative flex flex-col gap-5vh'>
+        <main className='main-layout relative gap-5vh'>
           {children}
         </main>
         <AppFooter />
